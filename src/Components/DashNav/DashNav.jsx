e4acebf0304c0825e2c9ebbe17ac8d966b9a5efb
Ashfakul Islam <r4pido3@gmail.com>
2023-12-17 01:31:26 +0600

    some navbar created for dashboard

--- src/Components/DashNav/DashNav.jsx
+++ src/Components/DashNav/DashNav.jsx
@@ -1,10 +1,49 @@
+import { NavLink } from "react-router-dom";
+import useAuth from "../../Hooks/useAuth/useAuth";
+import Swal from "sweetalert2";
+
 const DashNav = () => {
+  const { user, logout } = useAuth();
+  const handleSignOut = () => {
+    logout()
+      .then(() => {
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "Logged out",
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      })
+      .catch((err) => {
+        Swal.fire({
+          position: "center",
+          icon: "error",
+          title: `${err.message}`,
+          showConfirmButton: false,
+          timer: 2500,
+        });
+      });
+  };
+
+  const DashLink = (
+    <>
+      <li>
+        <NavLink to={'/'}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={'/dashboard/dashboard'}>DashBoard</NavLink>
+      </li>
+      <button className=" px-3 rounded-xl py-1 glass bg-red-600 hover:bg-red-800 text-white" onClick={handleSignOut}>Logout</button>
+    </>
+  );
+
   return (
     <div className="drawer z-50">
       <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content flex flex-col">
         {/* Navbar */}
-        <div className="w-full navbar bg-base-300">
+        <div className="w-full navbar bg-black bg-opacity-50">
           <div className="flex-none lg:hidden">
             <label
               htmlFor="my-drawer-3"
@@ -26,16 +65,14 @@ const DashNav = () => {
               </svg>
             </label>
           </div>
-          <div className="flex-1 px-2 mx-2"><p className="text-2xl font-bold">Dash<span className="text-red-500">Board</span></p></div>
+          <div className="flex-1 px-2 mx-2">
+            <p className="text-2xl font-bold text-white">
+              Dash<span className="text-red-500">Board</span>
+            </p>
+          </div>
           <div className="flex-none hidden lg:block">
-            <ul className="menu menu-horizontal">
-              {/* Navbar menu content here */}
-              <li>
-                <a>Navbar Item 1</a>
-              </li>
-              <li>
-                <a>Navbar Item 2</a>
-              </li>
+            <ul id="sidebar" className="menu text-white text-xl space-x-3 menu-horizontal">
+              {DashLink}
             </ul>
           </div>
         </div>
@@ -47,15 +84,7 @@ const DashNav = () => {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <ul className="menu p-4 w-72 min-h-full bg-base-200">
-          {/* Sidebar content here */}
-          <li>
-            <a>Sidebar Item 1</a>
-          </li>
-          <li>
-            <a>Sidebar Item 2</a>
-          </li>
-        </ul>
+        <ul className="menu p-4 w-72 min-h-full bg-base-200">{DashLink}</ul>
       </div>
     </div>
   );
